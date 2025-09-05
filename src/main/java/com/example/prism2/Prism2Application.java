566a1afabb25e0d4c103d0b5104b303469586420
yousef1110ya <yousefalshikhali1711@gmail.com>
2025-09-06 01:01:01 +0300

    initial commit
    in this commit I've just added the needed dependacies and create a
    docker-compose file for building a container for postegresql .

---
+++ src/main/java/com/example/prism2/Prism2Application.java
@@ -0,0 +1,13 @@
+package com.example.prism2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prism2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prism2Application.class, args);
+	}
+
+}
